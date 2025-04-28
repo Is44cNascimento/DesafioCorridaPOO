b68cced0e3a66f2625e48e857061e2585765f60b
Is44cNascimento <isaacgamex7@outlook.com>
2025-04-28 18:27:23 -0300

    Subindo projeto completo e finalizado 28-04-25 !!!!

--- src/Pista.java
+++ src/Pista.java
@@ -1,88 +1,71 @@
 import Carros.Carro;
 
-
 public class Pista {
+
+    private int tamanhoDaPista;
+
+    public Pista(int tamanhoDaPista) {
+        this.tamanhoDaPista = tamanhoDaPista;
+    }
+
+    public int getTamanhoDaPista() {
+        return tamanhoDaPista;
+    }
+
+    public void setTamanhoDaPista(int tamanhoDaPista) {
+        this.tamanhoDaPista = tamanhoDaPista;
+    }
+
     Carro carroUm = new Carro("Carro Um");
     Carro carroDois = new Carro("carro Dois");
 
+    public void visual() {
 
-    private int distanciaCarroUm;
-    private int distanciaCarroDois;
+        System.out.println("INICIANDO CORRIDA DE ASTERISCO");
+        System.out.println();
 
-    public int getDistanciaCarroUm() {
-        return this.distanciaCarroUm;
-    }
-
-    public void setDistanciaCarroUm(int distanciaCarroUm) {
-        this.distanciaCarroUm = distanciaCarroUm;
-    }
-
-    public int getDistanciaCarroDois() {
-        return this.distanciaCarroDois;
-    }
-
-    public void setDistanciaCarroDois(int distanciaCarroDois) {
-        this.distanciaCarroDois = distanciaCarroDois;
-    }
-
-
-
-    public void frontEnd() {
-
-    System.out.println("INICIANDO CORRIDA DE ASTERISCO");
-
-
-        String tamanhoDaPista="------------------------------";
+        String visualDaPista = "";
+        for (int i = 0; i < this.tamanhoDaPista; i++) {
+            visualDaPista += "-";
+        }
+        System.out.println(visualDaPista);
+        System.out.println("#");
+        System.out.println("#");
+        System.out.println(visualDaPista);
+        System.out.println();
 
         do{
+            carroUm.corridaIniciar();
+            carroDois.corridaIniciar();
 
-                    carroUm.corridaIniciar();
-                    carroDois.corridaIniciar();
-
-                    if(carroUm.getDistancia() > tamanhoDaPista.length()){
-                        carroUm.setDistancia(tamanhoDaPista.length());
-                    }
-                    if(carroDois.getDistancia() > tamanhoDaPista.length()){
-                        carroDois.setDistancia(tamanhoDaPista.length());
-                    }
-
-                System.out.println(tamanhoDaPista);
-                for(int i = 0; i < carroUm.getDistancia(); i++){
-
-
-                    setDistanciaCarroUm(carroUm.getDistancia());
-                    System.out.print(" ");
-                }
-                System.out.println("#");
-
-
-
-                for(int i = 0; i < carroDois.getDistancia(); i++){
+            System.out.println(visualDaPista);
+            for(int i = 0; i < carroUm.getDistancia(); i++){
                 System.out.print(" ");
-                setDistanciaCarroDois(carroDois.getDistancia());
             }
-                System.out.println("#");
+            System.out.println("#");
 
+            for(int i = 0; i < carroDois.getDistancia(); i++){
+                System.out.print(" ");
+            }
+            System.out.println("#");
 
-                System.out.println(tamanhoDaPista + "\n");
+            System.out.println(visualDaPista);
+            System.out.println();
 
-            }while (distanciaCarroDois <= 20 || distanciaCarroUm <= 20);
+        }while (carroUm.getDistancia() < this.tamanhoDaPista && carroDois.getDistancia() < this.tamanhoDaPista);
 
-                if(distanciaCarroUm  > distanciaCarroDois){
-                    carroUm.venceuIniciar();
+        System.out.println("Distancias Percorridas");
+        System.out.println(carroUm.getNomeDocarro() + ": " + carroUm.getDistancia());
+        System.out.println(carroDois.getNomeDocarro()+ ": " + carroDois.getDistancia());
 
-                }if(distanciaCarroDois > distanciaCarroUm){
-                    carroDois.venceuIniciar();
-            } if(distanciaCarroUm == distanciaCarroDois){
-                    System.out.println("EMPATE!!!");
-        }
+        if(carroUm.getDistancia()  > carroDois.getDistancia()){
+            carroUm.vencedor();
+        }else if(carroDois.getDistancia() > carroUm.getDistancia()){
+            carroDois.vencedor();
+        }else {
+            System.out.println("EMPATE!!!");
         }
 
     }
 
-
-
-
-
-
-
+}
--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
 
-        Pista pista = new Pista();
+        Pista pista = new Pista(30);
 
-            pista.frontEnd();
-
+        pista.visual();
     }
 }
